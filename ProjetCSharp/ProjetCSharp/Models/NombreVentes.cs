27bb0ca8d0665ee22eba58b7a0fb4fe83796167f
Abdoul Ahad Mbacke DIOUF <abdoul.diouf2@groupe-esigelec.org>
2024-03-12 10:13:55 +0100

    modif Classe nbvente

--- ProjetCSharp/ProjetCSharp/Models/NombreVentes.cs
+++ ProjetCSharp/ProjetCSharp/Models/NombreVentes.cs
@@ -1,6 +1,11 @@
-﻿namespace ProjetCSharp.Models
+﻿using System;
+
+namespace ProjetCSharp.Models
 {
     public class NombreVentes
     {
-    }
+        public int Id { get; set; }
+        public int Nbventes { get; set; }
+        public string Annee { get; set; }
+}
 }
